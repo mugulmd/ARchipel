1d5b13d19620520edae9727e3ea8cee3e1f89be1
Forwindz <dfxxlyc@126.com>
2021-12-14 02:43:06 +0100

    Add components for characters

---
+++ ARchipel/Assets/Scripts/Island/FloatIsland.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatIsland : IslandElement
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init("Target Rorie");  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ARchipel/Assets/Scripts/Island/TempleIsland.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempleIsland : IslandElement
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init("Target Rorie");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
