b24831926ec28cf2a0029316b49cb86811404758
Forwindz <dfxxlyc@126.com>
2021-12-14 02:52:49 +0100

    Forget to commit...

--- ARchipel/Assets/Scripts/Island/ForestIsland.cs
+++ ARchipel/Assets/Scripts/Island/ForestIsland.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ForestIsland : MonoBehaviour
+public class ForestIsland : IslandElement
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Init("Target Estelle");
     }
 
     // Update is called once per frame
